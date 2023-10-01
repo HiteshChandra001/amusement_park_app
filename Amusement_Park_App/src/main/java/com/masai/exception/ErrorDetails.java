7d15b98a158d4a4603691941b035d10dddf22174
Ravin Kumar Jangir <ravinjangir.rj1100@gmail.com>
2023-10-01 17:45:25 +0530

    day-3

---
+++ Amusement_Park_App/src/main/java/com/masai/exception/ErrorDetails.java
@@ -0,0 +1,21 @@
+package com.masai.exception;
+
+import java.time.LocalDateTime; 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ErrorDetails {
+
+	private LocalDateTime timeStamp;
+	private String message;
+	private String uri;
+	
+}
---
+++ Amusement_Park_App/src/main/java/com/masai/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.masai.exception;
+
+public class NotFoundException extends RuntimeException {
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
